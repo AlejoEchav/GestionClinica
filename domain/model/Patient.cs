46e4e0e4b5dcdd81ef655f528c995cfdfc6c3a76
AlejoEchav <alejoechavarria123@gmail.com>
2025-11-17 20:57:12 -0500

    Agregar archivos de proyecto.

---
+++ domain/model/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClinica.domain.model
+{
+    public class Patient : Person
+    {
+        // Información de Seguro 
+        public string InsuranceCompany { get; set; }
+        public string PolicyNumber { get; set; }
+        public bool IsPolicyActive { get; set; } // "Estado de la póliza: booleano"
+        public DateTime PolicyExpiration { get; set; }
+
+        // Contacto de Emergencia [cite: 49]
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContactPhone { get; set; } // "10 dígitos"
+        public string EmergencyContactRelation { get; set; }
+
+        // Acumulado de copagos del año (Para la regla del millón de pesos) [cite: 68]
+        public decimal YearlyCopayAccumulated { get; set; }
+    }
+}
---
+++ domain/model/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClinica.domain.model
+{
+    // Enum para limitar los roles exactos que pide el PDF 
+    public enum UserRole
+    {
+        HumanResources,
+        AdminStaff,
+        ItSupport,
+        Nurse,
+        Doctor
+    }
+
+    public class User : Person
+    {
+        public string Username { get; set; }
+        public string Password { get; set; } // En la vida real esto se encripta
+        public UserRole Role { get; set; }
+
+        // Constructor para obligar a tener datos mínimos
+        public User(string id, string name, string username, string password, UserRole role)
+        {
+            Id = id;
+            FullName = name;
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+    }
+}
