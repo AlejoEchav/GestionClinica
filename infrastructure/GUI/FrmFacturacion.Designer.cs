0e1bf057f860ec68eeacca259da75b6a298492b2
AlejoEchav <alejoechavarria123@gmail.com>
2025-11-19 14:58:15 -0500

    Formulario Facturación

---
+++ infrastructure/GUI/FrmFacturacion.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace GestionClinica.infrastructure.GUI
+{
+    partial class FrmFacturacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtBuscarCedula = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(46, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Cedula del Paciente";
+            label1.Click += label1_Click;
+            // 
+            // txtBuscarCedula
+            // 
+            txtBuscarCedula.Location = new Point(52, 90);
+            txtBuscarCedula.Name = "txtBuscarCedula";
+            txtBuscarCedula.Size = new Size(125, 27);
+            txtBuscarCedula.TabIndex = 1;
+            // 
+            // FrmFacturacion
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtBuscarCedula);
+            Controls.Add(label1);
+            Name = "FrmFacturacion";
+            Text = "FrmFacturacion";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtBuscarCedula;
+    }
+}
